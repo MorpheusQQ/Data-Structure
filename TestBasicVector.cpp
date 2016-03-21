ab4e4fcfceeb5868bbc76a2c5d3a8bffbfdf4a78
MorpheusQQ <morpheusqq@gmail.com>
2016-03-21 23:02:36 +0800

    Added files via upload

---
+++ TestBasicVector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include "BasicVector.h"
+
+
+using namespace std;
+
+int main(){
+
+	BasicVector<int> iv(5); // vector of 5 integers
+	BasicVector<double> dv(20); // vector of 20 doubles
+	BasicVector<string> sv(10); // vector of 10 strings
+
+	iv[3] = 8;
+	dv[14] = 2.5;
+	sv[7] = "hello";	cout << iv[3] << "\n" << endl;	cout << dv[14] << "\n" << endl;	cout << sv[7] << "\n" << endl;
+
+	system("pause");
+
+
+	return EXIT_SUCCESS;
+
+}
